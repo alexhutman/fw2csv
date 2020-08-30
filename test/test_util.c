409dbd5ec8e08c774b388565bc0abe5b35a29d27
Alex <alexhutman@gmail.com>
2020-08-30 16:34:53 -0400

    fail test if couldn't load file

--- test/test_util.c
+++ test/test_util.c
@@ -19,7 +19,10 @@ int main(int argc, char *argv[]) {
 		printf("Adding values from test file to array...\n");
 
 		// You have to call this test from the base directory of the project for this file to load correctly
-		get_lens_from_file(&lengths, "test/lengths.txt");
+		if (get_lens_from_file(&lengths, "test/lengths.txt")) {
+				return 1;
+		}
+
 		printf("Size: %d, capacity: %d\n", lengths.size, lengths.capacity);
 
 		print_len_arr(&lengths);
